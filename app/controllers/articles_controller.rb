9a9350e2a75e7d4a6006098f01df17d229e03645
Naoto-Hamada <hamadanaoto@hamada-no-MacBook-Air.local>
2016-07-12 16:35:10 +0900

    ログインしていない場合、投稿画面に行けないようにredirect設定

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
     @articles = Article.order("created_at DESC").page(params[:page]).per(5)
   end
@@ -11,7 +13,12 @@ class ArticlesController < ApplicationController
     Article.create(article_params)
   end
 
+  private
   def article_params
     params.permit(:title, :image, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
