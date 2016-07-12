11a2af206a8a2a4a8abbbccf713003cba474ca43
Naoto-Hamada <hamadanaoto@hamada-no-MacBook-Air.local>
2016-07-12 17:43:11 +0900

    current_user.idの値をuser_idに入力する

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,24 @@
 class TweetsController < ApplicationController
+
+  before_action :move_to_index, except: :index
+
   def index
-    # @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
+    @tweets = Tweet.page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
   end
 
   def create
-    # Tweet.create(article_params)
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
-  def article_params
-    # params.permit(:title, :image, :text)
+  private
+  def tweet_params
+    params.permit(:image, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
