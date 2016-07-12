1caca8153c48a1061ea8a8727b0334f5ace3bcf2
Naoto-Hamada <hamadanaoto@hamada-no-MacBook-Air.local>
2016-07-12 16:00:19 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,33 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :articles, only: [:new, :create]
+  resources :tweets,   only: [:new, :create]
+  get 'tweets' => 'tweets#index'
   root 'articles#index'
 end
+
+
+# ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+#                  Prefix Verb   URI Pattern                    Controller#Action
+# ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+#         new_user_session GET    /users/sign_in(.:format)       devise/sessions#new
+#             user_session POST   /users/sign_in(.:format)       devise/sessions#create
+#     destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
+#            user_password POST   /users/password(.:format)      devise/passwords#create
+#        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
+#                          PATCH  /users/password(.:format)      devise/passwords#update
+#                          PUT    /users/password(.:format)      devise/passwords#update
+# cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
+#        user_registration POST   /users(.:format)               devise/registrations#create
+#    new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
+#   edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
+#                          PATCH  /users(.:format)               devise/registrations#update
+#                          PUT    /users(.:format)               devise/registrations#update
+#                          DELETE /users(.:format)               devise/registrations#destroy
+#                 articles POST   /articles(.:format)            articles#create
+#              new_article GET    /articles/new(.:format)        articles#new
+#                   tweets POST   /tweets(.:format)              tweets#create
+#                new_tweet GET    /tweets/new(.:format)          tweets#new
+#                     root GET    /                              articles#index
+# ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
