0bb4f1f048ce82bd4ba62fcaa2180a97cbed3e2f
Naoto-Hamada <hamadanaoto@hamada-no-MacBook-Air.local>
2016-07-12 20:49:21 +0900

    アソシエーション定義

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712082103) do
+ActiveRecord::Schema.define(version: 20160712114453) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "title",      limit: 65535
     t.text     "image",      limit: 65535
     t.text     "text",       limit: 65535
     t.string   "genre",      limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "tweet_id",   limit: 4
+    t.text     "text",       limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
   end
