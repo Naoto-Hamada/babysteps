f49024cf95e08d32de7318ea46826c9940171fa6
Naoto-Hamada <hamadanaoto@hamada-no-MacBook-Air.local>
2016-07-12 17:23:01 +0900

    tweetsテーブルに、user_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712080458) do
+ActiveRecord::Schema.define(version: 20160712082103) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "title",      limit: 65535
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160712080458) do
     t.text     "text",       limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "users", force: :cascade do |t|
