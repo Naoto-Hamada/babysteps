f72e99cb3704ad35cb9d9bdbfada4b62d11b237a
Naoto-Hamada <hamadanaoto@hamada-no-MacBook-Air.local>
2016-07-14 00:29:22 +0900

    createアクションを定義

--- app/controllers/wants_controller.rb
+++ app/controllers/wants_controller.rb
@@ -4,6 +4,11 @@ class WantsController < ApplicationController
   end
 
   def create
+    Want.create(title: want_params[:title], rate: want_params[:rate], text: want_params[:text], user_id: current_user.id)
   end
 
+  private
+  def want_params
+    params.permit(:title, :rate, :text)
+  end
 end
